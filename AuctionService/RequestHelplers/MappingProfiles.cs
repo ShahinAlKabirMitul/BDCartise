1fbea568dc037b6a0870221753e05f8d23f7f9d4
Shahin al kabir <mitul.cse@gmail.com>
2024-06-09 19:58:24 +0600

    Working on Auto mapper .

--- AuctionService/RequestHelplers/MappingProfiles.cs
+++ AuctionService/RequestHelplers/MappingProfiles.cs
@@ -1,3 +1,5 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 
 namespace AuctionService.RequestHelplers;
@@ -6,6 +8,12 @@ public class MappingProfiles: Profile
 {
     public MappingProfiles()
     {
-        
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(d => d.Item, 
+                o=>
+                    o.MapFrom(s=>s));
+        CreateMap<CreateAuctionDto, Item>();
     }
 }
